b90cb2a9936c8d4eb8e9e5829b4416b113430af3
ChoccyMilkBoi <armijoemilio08@gmail.com>
2025-01-26 15:27:01 -0600

    Yes

--- Webmaster_24-25/src/Topbar.tsx
+++ Webmaster_24-25/src/Topbar.tsx
@@ -1,25 +1,38 @@
-function Topbar(){
-    return(
-        <ul>
-        <li>	
-            <a href="index.html">Home</a>
-        </li>
-        <li>
-            <a href="menu.html">Menu</a>
-        </li>
-        <li>
-            <a href="reservations.html">Reservations</a>
-        </li>
-        <li>
-            <a href="catering.html">Catering</a>
-        </li>
-        <li>
-            <a href="sources.html">Our sources</a>
-        </li>
-        <li>
-            <a href="mission.html">Our Mission</a>
-        </li>
-    </ul>
-    );
+import logo from "./assets/Black and White Retro Bakery Logo.svg.svg";
+
+function Topbar() {
+	return (
+		<ul className="streamerBTW">
+			<li className="buttonBTW">
+				<img src={logo} className="logoBTW"></img>
+				<a href="index.html" className="buttonlnkBTW"></a>
+			</li>
+			<li className="buttonBTW">
+				<a href="menu.html" className="buttonlnkBTW">
+					Menu
+				</a>
+			</li>
+			<li className="buttonBTW">
+				<a href="reservations.html" className="buttonlnkBTW">
+					Reservations
+				</a>
+			</li>
+			<li className="buttonBTW">
+				<a href="catering.html" className="buttonlnkBTW">
+					Catering
+				</a>
+			</li>
+			<li className="buttonBTW">
+				<a href="sources.html" className="buttonlnkBTW">
+					Our sources
+				</a>
+			</li>
+			<li className="buttonBTW">
+				<a href="mission.html" className="buttonlnkBTW">
+					Our Mission
+				</a>
+			</li>
+		</ul>
+	);
 }
 export default Topbar;
