ac2c815dff8582e4faa79f24d4f8f5dc8c23a319
ChoccyMilkBoi <armijoemilio08@gmail.com>
2025-01-26 21:31:08 -0600

    blah blah I got the logo

--- Webmaster_24-25/src/Topbar.tsx
+++ Webmaster_24-25/src/Topbar.tsx
@@ -3,32 +3,40 @@ import logo from "./assets/Black and White Retro Bakery Logo.svg.svg";
 function Topbar() {
 	return (
 		<ul className="streamerBTW">
-			<li className="buttonBTW">
-				<img src={logo} className="logoBTW"></img>
-				<a href="index.html" className="buttonlnkBTW"></a>
+			<li className="logo-buttonBTW">
+				<a href="index.html" className="logo-link">
+					<br></br>
+					<img src={logo} className="logoBTW logo"></img>
+					<span className="logo-text">Home</span>
+				</a>
 			</li>
 			<li className="buttonBTW">
 				<a href="menu.html" className="buttonlnkBTW">
+					<br></br>
 					Menu
 				</a>
 			</li>
 			<li className="buttonBTW">
 				<a href="reservations.html" className="buttonlnkBTW">
+					<br></br>
 					Reservations
 				</a>
 			</li>
 			<li className="buttonBTW">
 				<a href="catering.html" className="buttonlnkBTW">
+					<br></br>
 					Catering
 				</a>
 			</li>
 			<li className="buttonBTW">
 				<a href="sources.html" className="buttonlnkBTW">
+					<br></br>
 					Our sources
 				</a>
 			</li>
 			<li className="buttonBTW">
 				<a href="mission.html" className="buttonlnkBTW">
+					<br></br>
 					Our Mission
 				</a>
 			</li>
