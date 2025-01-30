06108ae80b8a843cd0f127d1c0c344552f23d846
Emilio <armijoemilio08@gmail.com>
2025-01-29 19:45:37 -0600

    ONE MOREMORE

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -9,123 +9,113 @@ function menu() {
 			<div className="full-grid-2">
 				<div className="menu-grid">
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Bruschetta al Pomodoro"
+						price={6.99}
 						posx={1}
 						posy={4}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						Crispy toasted bread topped with a mix of tomatoes, basil, garlic, and a drizzle of virgin olive oil.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Vegan Croquettes"
+						price={8.49}
 						posx={4}
 						posy={4}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						Golden, crispy bites filled with a blend of seasonal vegetables, herbs, and creamy mashed potatoes. Lightly seasoned and served with a zesty dipping sauce.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Panzanella Salad"
+						price={9.99}
 						posx={1}
 						posy={8}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						A Tuscan bread salad featuring tomatoes, cucumbers, red onions, and basil, all tossed with toasted bread and a light vinaigrette.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Greek Salad"
+						price={8.99}
 						posx={4}
 						posy={8}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						A blend of cucumbers, tomatoes, red onions, and Kalamata olives, topped with feta cheese and drizzled with herb-infused olive oil dressing.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Vegan Risotto al Funghi"
+						price={14.99}
 						posx={1}
 						posy={14}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						A creamy risotto crafted with Arborio rice, simmered in a vegetable broth, and infused with mushrooms, garlic, and herbs.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Ratatouille Provençale"
+						price={13.99}
 						posx={4}
 						posy={14}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						A traditional French dish featuring eggplant, zucchini, bell peppers, and tomatoes, slow-cooked with garlic, onions, and herbs.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Vegan Moussaka"
+						price={15.99}
 						posx={1}
 						posy={18}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						A blend of roasted eggplant, zucchini, and a lentil filling, finished with a dairy-free béchamel and baked.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Vegan Schnitzel"
+						price={14.49}
 						posx={4}
 						posy={18}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						A crispy breaded cutlet made from plant-based ingredients, perfectly seasoned and pan-fried.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Patatas Bravas"
+						price={4.99}
 						posx={1}
 						posy={24}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						Crispy, golden fried potatoes drizzled with a smoky tomato sauce and garlic aioli.
 						</p>
 					</MenuItem>
 					<MenuItem
-						name="ChoppedChyn"
-						price={3.5}
+						name="Sauteed Spinish with Garlic and Olive Oil"
+						price={5.99}
 						posx={4}
 						posy={24}
 						pic="Webmaster_24-25/src/assets/services-pic.jpg"
 					>
 						<p>
-							Chopped Chyn is a delicious salad with a variety of
-							ingredients.
+						Fresh spinach leaves  sautéed in garlic and extra virgin olive oil, seasoned to perfection.
 						</p>
 					</MenuItem>
 					<MenuItem
